beaec9c4a2d90185950a003da3b3cfdf65bdad0e
abel <abel93lk@gmail.com>
2020-02-04 13:58:49 -0600

    creacion IF y FOR

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class BodyComponent{
-    
+    mostrar = true;
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Peter Parker'
+    };
+    refrescos: string[] = ['coca-cola', 'pepsi', 'mirinda'];
 }
