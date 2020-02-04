25821eda8ce7d182e7f318970babb1a6f8ae3a36
abel <abel93lk@gmail.com>
2020-02-04 13:15:58 -0600

    creando header, body, footer

--- 01-hola-mundo/src/app/app.module.ts
+++ 01-hola-mundo/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // comienza codigo
 import { HeaderComponent} from './components/header/header.componet';
+import {BodyComponent} from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- 01-hola-mundo/src/app/components/header/header.componet.ts
+++ 01-hola-mundo/src/app/components/header/header.componet.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `<h1>header html</h1>`
+    templateUrl: './header.component.html'
 })
 
  export class HeaderComponent{
